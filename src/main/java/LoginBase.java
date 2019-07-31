77a050f9eebcb8c0f25c38fee8f41358983f3457
mike li <mikesiwer01@gmail.com>
2019-07-31 11:16:42 -0400

    update

--- src/main/java/LoginBase.java
+++ src/main/java/LoginBase.java
@@ -1,9 +1,20 @@
-//public class LoginBase {
-//
-//    private String login(){
-//        baseUrl
-//
-//    }
-//
-//
-//}
+public class LoginBase {
+
+
+    private String baseUrl = "https://nbai.io/dashboard/#/login";
+    private String userName = "mikesiwer10@gmail.com";
+    private String password = "A123456";
+
+    public String getBaseUrl(){
+        return baseUrl;
+    }
+
+    public String getUserName(){
+        return userName;
+    }
+
+    public  String getPassword(){
+        return password;
+    }
+
+}
--- src/main/java/SeleniumUITest.java
+++ src/main/java/SeleniumUITest.java
@@ -8,7 +8,9 @@ public class SeleniumUITest {
 
     public void login() {
 //        LoginBase lb = new LoginBase();
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\win10\\IdeaProjects\\AutomationTestingJAVA\\src\\main\\java\\chromedriver.exe");
+
+//        System.setProperty("webdriver.chrome.driver","C:\\Users\\win10\\IdeaProjects\\AutomationTestingJAVA\\src\\main\\java\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.get("https://nbai.io/dashboard/#/login");
         driver.manage().window().maximize();
@@ -20,6 +22,28 @@ public class SeleniumUITest {
             e.printStackTrace();
         }
 
+        String baseUrl = "http://demo.guru99.com/test/newtours/";
+        String expectedTitle = "Welcome: Mercury Tours";
+        String actualTitle = "";
+
+        // launch Fire fox and direct it to the Base URL
+        driver.get(baseUrl);
+
+        // get the actual value of the title
+        actualTitle = driver.getTitle();
+
+        /*
+         * compare the actual title of the page with the expected one and print
+         * the result as "Passed" or "Failed"
+         */
+        if (actualTitle.contentEquals(expectedTitle)){
+            System.out.println("Test Passed!");
+        } else {
+            System.out.println("Test Failed");
+        }
+
+        //close Fire fox
+        driver.close();
         //login
         driver.findElement(By.xpath("/html/body")).click();
 //        String loginPage = lb.getBaseUrl();
